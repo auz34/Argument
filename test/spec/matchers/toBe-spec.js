daf7169cff4a171b3d55a9980bbabd32eff109a9
Andrew Zavgorodniy <andrewzavgorodniy@gmail.com>
2015-05-07 23:53:25 -0700

    add toBe spec

---
+++ test/spec/matchers/toBe-spec.js
@@ -0,0 +1,14 @@
+var unit = require('../../../src/expectation/matchers/toBe'),
+    toBe = unit()().compare;
+
+describe('toBe', function() {
+    it('should return pass when actual strictly equal to expected', function() {
+        var result = toBe('a', 'a');
+        expect(result.pass).toBe(true);
+    });
+
+    it('should return pass when actual strictly equal to expected', function() {
+        var result = toBe('1', 1);
+        expect(result.pass).toBe(false);
+    });
+});
