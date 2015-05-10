ddbcb6db7850df19ff49ee7b0357d2d0fa3aaea0
Andrew Zavgorodniy <andrewzavgorodniy@gmail.com>
2015-05-10 00:57:06 -0700

    additional tests for expectations

--- test/spec/args-expectation-spec.js
+++ test/spec/args-expectation-spec.js
@@ -10,6 +10,18 @@ describe('expectation', function() {
         result = [];
     });
 
+    it('should return one result per one expect evaluation', function() {
+        var expectObj = expectationFactory({
+            actual: 5,
+            reporters: [reporter]
+        });
+
+        expectObj.toBe(6);
+        expect(result.length).toBe(1);
+        expectObj.toBe(5);
+        expect(result.length).toBe(2);
+    });
+
     it('should be able to see positive result', function() {
         var expectObj = expectationFactory({
             actual: 5,
@@ -17,8 +29,6 @@ describe('expectation', function() {
         });
 
         expectObj.toBe(5);
-
-        expect(result.length).toBe(1);
         expect(result[0].pass).toBe(true);
     });
 
@@ -29,7 +39,29 @@ describe('expectation', function() {
         });
 
         expectObj.toBe(5);
-        expect(result.length).toBe(1);
         expect(result[0].pass).toBe(false);
     });
+
+    it('should negate result if "isNot" option is included', function() {
+        var expectObj = expectationFactory({
+            actual: 't',
+            reporters: [reporter],
+            isNot: true
+        });
+
+        expectObj.toBe('a');
+        expect(result[0].pass).toBe(true);
+    });
+
+    it('should support fluent property for negation', function() {
+        var expectObj = expectationFactory({
+            actual: 'd',
+            reporters: [reporter]
+        });
+
+        expectObj.not.toBe('a');
+        expect(result[0].pass).toBe(true);
+    });
+
+
 });
